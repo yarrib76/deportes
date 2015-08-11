e0322f96c210f73f96daa4a1ffc32c0f3a020e51
Yamil <yarrib76@gmail.com>
2015-08-11 11:25:21 -0300

    Creo css para el fullcalendar

--- resources/views/agenda/calendario1.blade.php
+++ resources/views/agenda/calendario1.blade.php
@@ -25,6 +25,8 @@
 @section('extra-javascript')
     <link href='/fullcalendar/fullcalendar.css' rel='stylesheet' />
     <link href='/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print' />
+    <link href='/css/librerias/fullcalendar/calendar.css' rel='stylesheet' />
+
     <script src='/fullcalendar/lib/moment.min.js'></script>
     <script src='/fullcalendar/lib/jquery.min.js'></script>
     <script src='/fullcalendar/lib/jquery-ui.custom.min.js'></script>
@@ -148,55 +150,3 @@ $('#calendar').fullCalendar({
 });
     </script>
 @stop
-<style>
-
-    body {
-        margin-top: 40px;
-        text-align: center;
-        font-size: 14px;
-        font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
-    }
-
-    #wrap {
-        width: 1100px;
-        margin: 0 auto;
-    }
-
-    #external-events {
-        float: left;
-        width: 150px;
-        padding: 0 10px;
-        border: 1px solid #ccc;
-        background: #eee;
-        text-align: left;
-    }
-
-    #external-events h4 {
-        font-size: 16px;
-        margin-top: 0;
-        padding-top: 1em;
-    }
-
-    #external-events .fc-event {
-        margin: 10px 0;
-        cursor: pointer;
-        background-color: #3a87ad;
-    }
-
-    #external-events p {
-        margin: 1.5em 0;
-        font-size: 11px;
-        color: #666;
-    }
-
-    #external-events p input {
-        margin: 0;
-        vertical-align: middle;
-    }
-
-    #calendar {
-        float: right;
-        width: 900px;
-    }
-
-</style>
