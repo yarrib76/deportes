1eb2b6447f8ecee1e0a5a81772e4e536f07442b5
Yamil <yarrib76@gmail.com>
2015-09-08 16:51:25 -0300

    cambios

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,9 @@
 <html lang="es">
 {{--Head--}}
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Deportes</title>
     <link href="/css/app.css" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Font Awesome -->
     <link href="/css/librerias/font-awesome/font-awesome.css" rel="stylesheet">
