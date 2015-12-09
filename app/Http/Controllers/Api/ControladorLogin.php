68319064e022630c04431ed2ed9f9aabfd609b0c
Yamil <yarrib76@gmail.com>
2015-12-09 16:11:12 -0300

    Agrego Api para login desde Moviles

---
+++ app/Http/Controllers/Api/ControladorLogin.php
@@ -0,0 +1,32 @@
+<?php namespace Deportes\Http\Controllers\Api;
+
+use Deportes\Http\Requests;
+use Deportes\Http\Controllers\Controller;
+
+use Deportes\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
+
+class ControladorLogin extends Controller {
+    public function loginUsuarios(){
+        $email = Input::get('email');
+        $password = Input::get('password');
+        $esValido = $this->validacion($email,$password);
+        return Response::json($esValido);
+    }
+
+    public function validacion($email,$password){
+        $repuesta=[];
+        $datos = User::where("email",$email)->first();
+        if (!$datos){
+            return $repuesta[0] = ['valor' => 'El usuario no existre'];
+        }else{
+            if (Hash::check($password, $datos->password)){
+                return $repuesta[0] = ['valor' => $datos->id];
+            }
+            return $repuesta[0]= ['valor' => 'Password Incorrecto'];
+        }
+    }
+
+}
